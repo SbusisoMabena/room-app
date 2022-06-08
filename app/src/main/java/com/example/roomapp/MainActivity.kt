7793dba309ae10ca57399160e4dc7d53cce859bf
Sbusiso Mabena <Sbusisomabena0@gmail.com>
2022-06-08 19:10:11 +0200

    feat: save data to the database

--- app/src/main/java/com/example/roomapp/MainActivity.kt
+++ app/src/main/java/com/example/roomapp/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.roomapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupActionBarWithNavController(findNavController(R.id.fragment))
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        setupActionBarWithNavController(navHostFragment.navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = navHostFragment.navController
+
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/roomapp/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/roomapp/fragments/add/AddFragment.kt
@@ -1,19 +1,63 @@
 package com.example.roomapp.fragments.add
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.roomapp.R
+import com.example.roomapp.data.User
+import com.example.roomapp.data.UserViewModel
+import com.example.roomapp.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
+    private lateinit var userViewModel: UserViewModel
+
+    private var _binding: FragmentAddBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        _binding = FragmentAddBinding.inflate(inflater, container, false)
+
+        val view = binding.root
+
+        userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        binding.addBtn.setOnClickListener {
+            insertDataToDatabase()
+        }
+
+        return view
+    }
+
+    private fun insertDataToDatabase() {
+        val firstName = binding.addFirstNameEt.text.toString()
+        val lastName = binding.addLastNameEt.text.toString()
+        val age = binding.addAgeEt.text
+
+        if (inputCheck(firstName, lastName, age)) {
+            val user = User(0, firstName, lastName, Integer.parseInt(age.toString()))
+
+            userViewModel.addUser(user)
+            Toast.makeText(requireContext(), "Successfully added!", Toast.LENGTH_LONG).show()
+
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(), "Please fill out all fields", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean {
+        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
     }
 
 }
